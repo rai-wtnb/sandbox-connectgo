c78966c725d08de029b2478812930c26926fbb9d
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2022-09-28 14:14:17 +0900

    fix

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	ctx := context.Background()
 
-	client := greetv1connect.NewGreetServiceClient(http.DefaultClient, "http://localhost:8080", connect.WithGRPC())
+	client := greetv1connect.NewGreetServiceClient(http.DefaultClient, "http://localhost:8080")
 	res, err := client.Greet(ctx, connect.NewRequest(&greetv1.GreetRequest{Name: "Jane"}))
 	if err != nil {
 		log.Println(err)
